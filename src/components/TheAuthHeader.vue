036f8e97905cbea5ced8824865924d7fd187099c
Guillermo García <guillermieres97@gmail.com>
2022-10-11 00:10:34 +0200

    create login and register view and auth header

---
+++ src/components/TheAuthHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "TheAuthHeader",
+};
+</script>
+
+<template>
+  <header class="d-flex justify-content-center align-items-center pt-3 pb-3">
+    <nav class="d-flex justify-content-between align-items-center w-75">
+      <a href="/"><h4 id="title" class="fw-bold">The Eyed Traveller</h4></a>
+      <h1 class="fw-bold fs-2">Find your next travel</h1>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+@import "../assets/main.css";
+
+header {
+  color: white;
+  background-color: var(--base-color-blue);
+  width: 100%;
+}
+
+#title {
+  cursor: pointer;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<template>This is the food view</template>
+
+<style></style>
