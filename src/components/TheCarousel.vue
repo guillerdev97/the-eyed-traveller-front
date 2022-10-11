7ca2e8bef0600a2acd7d3c5c635b0c52b551e781
Guillermo García <guillermieres97@gmail.com>
2022-10-11 16:55:33 +0200

    create carousel component

---
+++ src/components/TheCarousel.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+  name: "TheCarousel",
+};
+</script>
+
+<template>
+  <div id="carousel" class="carousel slide border" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div
+        class="carousel-item active d-flex justify-content-between align-items-center"
+        data-bs-interval="2000"
+      >
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+      </div>
+
+      <div
+        class="carousel-item d-flex justify-content-between align-items-center"
+        data-bs-interval="2000"
+      >
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+        <div class="img d-flex justify-content-start align-items-end">
+          <p class="img-text">Text</p>
+        </div>
+      </div>
+    </div>
+
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carousel"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleControlsNoTouching"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+@import "../assets/main.css";
+
+#carousel {
+  width: 80vw;
+  height: 30vh;
+  margin: 9vh auto;
+}
+
+.img {
+  width: 17vw;
+  height: 30vh;
+  border: 1px solid black;
+}
+
+button span{
+   color: black;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <script>
 import TheHeader from "../components/TheHeader.vue";
 import TrendingHome from "../components/TrendingHome.vue";
+import TheCarousel from "../components/TheCarousel.vue";
 
 export default {
   name: "HomeView",
-  components: { TheHeader, TrendingHome },
+  components: { TheHeader, TrendingHome, TheCarousel },
 };
 </script>
 
 <template>
   <TheHeader />
   <TrendingHome />
+  <TheCarousel />
 </template>
 
 <style></style>
